63eecfc79634d6fbaf64f505ed4369153b1c741a
root <terslykifugo96m>
2022-03-11 04:43:28 -0800

    using putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - print alphabet in lowercase
- *
- * Return : Always 0 (Success)
- */
+  * main - Entry point
+  *
+  * Return : Always 0 (Success)
+  */
 
 int main(void)
+
 {
-	char lowchr;
+	char lowchr = 'a';
 
 	for (lowchr = 'a'; lowchr <= 'z'; lowchr++)
 	{
 		putchar(lowchr);
-		putchar('\n');
 	}
+
 	return (0);
 }
