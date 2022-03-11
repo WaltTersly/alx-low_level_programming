728f1599947de068b8f5cba92d994ef8aef49a60
root <terslykifugo96m>
2022-03-10 20:16:39 -0800

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *    * main - print to screen
+ *    *
+ *    * Return: zero
+ *    */
+int main(void)
+{
+	char Mystrng[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystrng);
+	return (0);
+}
