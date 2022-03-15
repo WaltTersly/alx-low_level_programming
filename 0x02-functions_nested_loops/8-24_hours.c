07a4a11306dce392b76e898930015938c7e7d39b
root <terslykifugo96m>
2022-03-15 11:53:59 -0700

    time countdown

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * jack_bauer - prints every minute of the day of Jack Bauer
+  *
+  * Return: Always 0.
+  */
+void jack_bauer(void)
+{
+	int hrs, mins;
+
+	hrs = 0;
+
+	while (hrs < 24)
+	{
+		mins = 0;
+		while (mins < 60)
+		{
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar(':');
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
