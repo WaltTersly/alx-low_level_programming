75cfca5b62f852cc84e2d93d3cb6363de64b0364
root <terslykifugo96m>
2022-03-15 21:08:21 -0700

    add functionality

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * add - adds two integers and returns the result
+  * @a: first interger
+  * @b: second interger
+  * Return: sum of a and b
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
