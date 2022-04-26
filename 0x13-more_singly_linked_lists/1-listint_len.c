fd284dd55322f9aea4c095cb85fa9a2c702c64ad
root <terslykifugo96m>
2022-04-26 11:43:17 -0700

    counts the nodes in a list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
+
 /**
- *  * listint_len - returns the number of elements in a linked lists
- *   * @h: linked list of type listint_t to traverse
- *    *
- *     * Return: number of nodes
- *      */
+ *  * listint_len - Calculates the length of a list
+ *   * @h: Pointer to the begining of the list
+ *    * Return: Returns the amount of nodes
+ *     */
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+	unsigned int count;
 
-	while (h)
-	{
-		num++;
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
 		h = h->next;
-	}
-	return (num);
+	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,16 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
  *  * struct listint_s - singly linked list
  *   * @n: integer
  *    * @next: points to the next node
  *     *
  *      * Description: singly linked list node structure
- *       * for project
+ *       * for Holberton project
  *        */
 typedef struct listint_s
 {
@@ -29,8 +30,5 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
 
 #endif
