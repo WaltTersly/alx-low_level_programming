8272cd3db90f571893015f24c998bef98800f336
root <terslykifugo96m>
2022-04-26 11:49:29 -0700

    count number of elements of a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
- *  * print_listint - Prints all elements of a listint_t list.
- *   *
- *    * @h: pointer to head.
- *     * Return: Number of nodes.
- *      */
+ *  * print_listint - Prints all elements of a list
+ *   * @h: Pointer to a list
+ *    * Return: The number of nodes in the list
+ *     */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
-
-	while (h)
+	unsigned int count;
+	
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
-		num++;
 		h = h->next;
 	}
-	return (num);
+	return (count);
 }
