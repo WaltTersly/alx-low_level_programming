a7fde33053a1843948c07d9282d6b2d52026c0f3
root <terslykifugo96m>
2022-04-29 19:46:19 -0700

    removing case where the index is already 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,13 +7,9 @@
  *      */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
-
-	mask = 1;
-	mask = mask << index;
-	if (index > sizeof(unsigned long int) * 8 || n == NULL)
+	if (index > 63)
 		return (-1);
-	if (((*n >> index) & 1) == 1) /*Checks if 1 at position*/
-		*n = mask ^ *n;
+
+	*n = (~(1UL << index) & *n);
 	return (1);
 }
