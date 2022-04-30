adb73e686a95780c7f82de3673dc75c64ece96af
root <terslykifugo96m>
2022-04-29 19:27:41 -0700

    prints binary representation of a numnber

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,24 +1,28 @@
 #include "main.h"
+void print_b(unsigned long int n);
 /**
- *  * print_binary - prints the binary equivalent of a number
- *   * @n: number to print in binary
+ *  * print_binary - Prints the binary representation of a number
+ *   * @n: Number to interpret as binary
  *    */
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
-	unsigned long int current;
+	if (n == 0)
+		_putchar('0');
+	else
+		print_b(n);
+}
 
-	for (i = 63; i >= 0; i--)
-	{
-		current = n >> 1;
-		if (current & 1)
-		{
-			_putchar('1');
-			count++;
-		}
-		else if (count)
-			_putchar('0');
-	}
-	if (!count)
+/**
+ *  * print_b - Prints the binary representation of a number
+ *   * @n: Number to interpret as binary
+ *    */
+void print_b(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	print_b((n >> 1));
+	if ((n & 1) == 1)
+		_putchar('1');
+	if ((n & 1) == 0)
 		_putchar('0');
 }
