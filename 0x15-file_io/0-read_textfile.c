ad8cbbdfa3d2a4b8b94ccae891da2591f323d2e9
root <terslykifugo96m>
2022-05-03 22:20:47 -0700

    read a text and file print it to POsix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,40 +8,24 @@
  *       */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i;
-	int read_letters;
+	int fd, chars_read, chars_written;
 	char *buf;
 
 	if (filename == NULL)
 		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		free(buf);
+	chars_read = read(fd, buf, letters);
+	if (chars_read == -1)
 		return (0);
-	}
-
-	read_letters = read(fd, buf, letters);
-	if (read_letters == -1)
-	{
-		close(fd);
-		free(buf);
+	chars_written = write(STDOUT_FILENO, buf, chars_read);
+	if (chars_written == -1)
 		return (0);
-	}
-	for (i = 0; i < read_letters; i++)
-	{
-		if (write(STDOUT_FILENO, &buf[i], 1) == -1)
-		{
-			close(fd);
-			free(buf);
-			return (0);
-		}
-	}
 	close(fd);
 	free(buf);
-	return (read_letters);
+	return (chars_written);
 }
